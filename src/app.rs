5c4719c2cf723bb8d754d7ee6f92ae704a0807fb
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-01-17 18:26:48 +0300

    Add files via upload

--- src/app.rs
+++ src/app.rs
@@ -1,92 +1,49 @@
-use serde::{Deserialize, Serialize};
-use serde_wasm_bindgen::to_value;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
+use web_sys::HtmlInputElement;
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
-    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+pub struct App {
+    link: Callback<web_sys::Event>,
+    input_ref: NodeRef,
 }
 
-#[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
+pub enum Msg {
+    FolderSelected,
 }
 
-#[function_component(App)]
-pub fn app() -> Html {
-    let greet_input_ref = use_node_ref();
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
 
-    let name = use_state(|| String::new());
-
-    let greet_msg = use_state(|| String::new());
-    {
-        let greet_msg = greet_msg.clone();
-        let name = name.clone();
-        let name2 = name.clone();
-        use_effect_with_deps(
-            move |_| {
-                spawn_local(async move {
-                    if name.is_empty() {
-                        return;
-                    }
-
-                    let args = to_value(&GreetArgs { name: &*name }).unwrap();
-                    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-                    let new_msg = invoke("greet", args).await.as_string().unwrap();
-                    greet_msg.set(new_msg);
-                });
-
-                || {}
-            },
-            name2,
-        );
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {
+            link: ctx.link().callback(|_| Msg::FolderSelected),
+            input_ref: NodeRef::default(),
+        }
     }
 
-    let greet = {
-        let name = name.clone();
-        let greet_input_ref = greet_input_ref.clone();
-        Callback::from(move |e: SubmitEvent| {
-            e.prevent_default();
-            name.set(
-                greet_input_ref
-                    .cast::<web_sys::HtmlInputElement>()
-                    .unwrap()
-                    .value(),
-            );
-        })
-    };
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::FolderSelected => {
+                if let Some(input) = self.input_ref.cast::<HtmlInputElement>() {
+                    if let Some(files) = input.files() {
+                        for i in 0..files.length() {
+                            let file = files.item(i).unwrap();
+                            log::info!("File selected: {:?}", file.name());
+                        }
+                    }
+                }
+            }
+        }
+        false
+    }
 
-    html! {
-        <main class="container">
-            <div class="row">
-                <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
-                </a>
-                <a href="https://yew.rs" target="_blank">
-                    <img src="public/yew.png" class="logo yew" alt="Yew logo"/>
-                </a>
-            </div>
-
-            <p>{"Click on the Tauri and Yew logos to learn more."}</p>
-
-            <p>
-                {"Recommended IDE setup: "}
-                <a href="https://code.visualstudio.com/" target="_blank">{"VS Code"}</a>
-                {" + "}
-                <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank">{"Tauri"}</a>
-                {" + "}
-                <a href="https://github.com/rust-lang/rust-analyzer" target="_blank">{"rust-analyzer"}</a>
-            </p>
-
-            <form class="row" onsubmit={greet}>
-                <input id="greet-input" ref={greet_input_ref} placeholder="Enter a name..." />
-                <button type="submit">{"Greet"}</button>
-            </form>
-
-            <p><b>{ &*greet_msg }</b></p>
-        </main>
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        html! {
+            <input type="file"
+                   multiple=true
+                   webkitdirectory={true.to_string()}
+                   ref={self.input_ref.clone()}
+                   onchange={self.link.clone()} />
+        }
     }
 }
